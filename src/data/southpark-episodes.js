fa7e9711e99358c45fd2f6648f92e613c52dfc48
SahajR <sahaj.ramachandran@gmail.com>
2017-10-20 02:04:20 +0530

    Season 21 episode 5 is now available to stream

--- src/data/southpark-episodes.js
+++ src/data/southpark-episodes.js
@@ -4,7 +4,7 @@ const seasonCount = 21;
 
 const episodesPerSeason = [0, 13, 18, 17, 17, 14, 17, 15, 14, 14, 14, 14, 14, 14, 14, 14, 14, 10, 10, 10, 10, 10];
 
-const unavailableEpisodes = ['s05e04', 's14e05', 's14e06', 's21e05', 's21e06', 's21e07', 's21e08', 's21e09', 's21e10'];
+const unavailableEpisodes = ['s05e04', 's14e05', 's14e06', 's21e06', 's21e07', 's21e08', 's21e09', 's21e10'];
 
 const getRandomEpisode = (season = prependZero(Math.floor(Math.random()*seasonCount)+1)) => {
     const seasonNumber = Number(season);
